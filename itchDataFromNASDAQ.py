579c9c6c4a601f2abaab698d0bda9281277448c8
Patrick Fien <pmfien99@icloud.com>
2022-05-26 16:35:18 -0400

    Updates

--- itchDataFromNASDAQ.py
+++ itchDataFromNASDAQ.py
@@ -19,7 +19,7 @@ import seaborn as sns
 warnings.filterwarnings('ignore')
 
 sns.set_style('whitegrid')
-FTP_URL = 'ftp://emi.nasdaq.com/ITCH/Nasdaq ITCH/'
+FTP_URL = 'https://emi.nasdaq.com/ITCH/Nasdaq%20ITCH/'
 SOURCE_FILE = '10302019.NASDAQ_ITCH50.gz'
 
 def format_time(t):
@@ -28,27 +28,28 @@ def format_time(t):
     return f'{h:0>2.0f}:{m:0>2.0f}:{s:0>5.2f}'
 
 def may_be_download(url):
+    """Download & unzip ITCH data if not yet available"""
     if not data_path.exists():
-        print('Creating Directory')
+        print('Creating directory')
         data_path.mkdir()
     else: 
         print('Directory exists')
 
     filename = data_path / url.split('/')[-1]        
     if not filename.exists():
-        print("Downloading ... ", url)
-        urlretrieve(url, "test")
+        print('Downloading...', url)
+        urlretrieve(url, filename)
     else: 
-        print("File exists")
-    
-    unzipped = data_path / (filename.stem + ".bin")
+        print('File exists')        
+
+    unzipped = data_path / (filename.stem + '.bin')
     if not unzipped.exists():
-        print("Unzipping to ", unzipped)
+        print('Unzipping to', unzipped)
         with gzip.open(str(filename), 'rb') as f_in:
-            with open(unzipped, 'wb') as f_out: 
+            with open(unzipped, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
     else: 
-        print("File already unpacked")
+        print('File already unpacked')
     return unzipped
 
 data_path = Path('data')
@@ -60,6 +61,7 @@ order_book_store = data_path / 'order_book.h5'
 file_name = may_be_download(urljoin(FTP_URL, SOURCE_FILE))
 date = file_name.name.split('.')[0]
 
+## Defining format strings ##
 event_codes = {'O': 'Start of Messages',
                'S': 'Start of System Hours',
                'Q': 'Start of Market Hours',
@@ -87,3 +89,134 @@ formats = {
     ('price_4', 4): 'I',
     ('price_8', 8): 'Q',
 }
+
+message_data = (pd.read_excel('message_types.xlsx',
+                              sheet_name='messages')
+                .sort_values('id')
+                .drop('id', axis=1))
+
+message_data.head()
+
+def clean_message_types(df):
+    df.columns = [c.lower().strip() for c in df.columns]
+    df.value = df.value.str.strip()
+    df.name = (df.name
+                .str.strip() # remove whitespace
+                .str.lower()
+                .str.replace(' ', '_')
+                .str.replace('-', '_')
+                .str.replace('/', '_'))
+    df.notes = df.notes.str.strip()
+    df['message_type'] = df.loc[df.name == 'message_type', 'value']
+    return df
+
+message_types = clean_message_types(message_data)
+
+#Get Message Labels
+#We extract message type codes and names so we can later make the results more readable.
+
+message_labels = (message_types.loc[:, ['message_type', 'notes']]
+.dropna()
+.rename(columns={'notes' : 'name'}))
+
+message_labels.name = (message_labels.name
+                       .str.lower()
+                       .str.replace('message', '')
+                       .str.replace('.', '')
+                       .str.strip().str.replace(' ', '_'))
+# message_labels.to_csv('message_labels.csv', index=False)
+message_labels.head()
+
+#Finalize specification details
+#Each message consists of several fields that are defined by offset, length and type of value. The struct module will use this format information to parse the binary source data.
+message_types.message_type = message_types.message_type.ffill()
+message_types = message_types[message_types.name != 'message_type']
+message_types.value = (message_types.value
+                       .str.lower()
+                       .str.replace(' ', '_')
+                       .str.replace('(', '')
+                       .str.replace(')', ''))
+message_types.info()
+
+message_types.head()
+
+message_types.to_csv('message_types.csv', index=False)
+message_types = pd.read_csv('message_types.csv')
+
+#The parser translates the message specs into format strings and 
+#namedtuples that capture the message content. 
+# First, we create (type, length) formatting tuples from ITCH specs:
+
+message_types.loc[:, 'formats'] = (message_types[['value', 'length']]
+                            .apply(tuple, axis=1).map(formats))
+
+# Then, we extract formatting details for alphanumerical fields
+alpha_fields = message_types[message_types.value == 'alpha'].set_index('name')
+alpha_msgs = alpha_fields.groupby('message_type')
+alpha_formats = {k: v.to_dict() for k, v in alpha_msgs.formats}
+alpha_length = {k: v.add(5).to_dict() for k, v in alpha_msgs.length}
+
+#We generate message classes as named tuples and format strings
+
+message_fields, fstring = {}, {}
+for t, message in message_types.groupby('message_type'):
+    message_fields[t] = namedtuple(typename=t, field_names=message.name.tolist())
+    fstring[t] = '>' + ''.join(message.formats.tolist())
+alpha_fields.info()
+alpha_fields.head()
+
+# Fields of alpha type (alphanumeric) require post-processing as defined in the format_alpha function:
+
+def format_alpha(mtype, data):
+    """Process byte strings of type alpha"""
+
+    for col in alpha_formats.get(mtype).keys():
+        if mtype != 'R' and col == 'stock':
+            data = data.drop(col, axis=1)
+            continue
+        data.loc[:, col] = data.loc[:, col].str.decode("utf-8").str.strip()
+        if encoding.get(col):
+            data.loc[:, col] = data.loc[:, col].map(encoding.get(col))
+    return data
+
+#The binary file for a single day contains over 350,000,000 messages worth over 12 GB.
+
+def store_messages(m):
+    """Handle occasional storing of all messages"""
+    with pd.HDFStore(itch_store) as store:
+        for mtype, data in m.items():
+            # convert to DataFrame
+            data = pd.DataFrame(data)
+
+            # parse timestamp info
+            data.timestamp = data.timestamp.apply(int.from_bytes, byteorder='big')
+            data.timestamp = pd.to_timedelta(data.timestamp)
+
+            # apply alpha formatting
+            if mtype in alpha_formats.keys():
+                data = format_alpha(mtype, data)
+
+            s = alpha_length.get(mtype)
+            if s:
+                s = {c: s.get(c) for c in data.columns}
+            dc = ['stock_locate']
+            if m == 'R':
+                dc.append('stock')
+            try:
+                store.append(mtype,
+                         data,
+                         format='t',
+                         min_itemsize=s,
+                         data_columns=dc)
+            except Exception as e:
+                print(e)
+                print(mtype)
+                print(data.info())
+                print(pd.Series(list(m.keys())).value_counts())
+                data.to_csv('data.csv', index=False)
+            return 1
+    return 0
+
+messages = defaultdict(list)
+message_count = 0
+message_type_counter = Counter()
